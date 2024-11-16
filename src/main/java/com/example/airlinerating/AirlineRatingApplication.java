ffc4aee9aeefb093dbf12ed9cd07f2a76b9ef868
edan.9303 <edan.9303@kakaocorp.com>
2024-11-16 22:54:54 +0900

    환경변수 추가

--- src/main/java/com/example/airlinerating/AirlineRatingApplication.java
+++ src/main/java/com/example/airlinerating/AirlineRatingApplication.java
@@ -1,15 +1,33 @@
 package com.example.airlinerating;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
 public class AirlineRatingApplication {
+    private static Environment environment;
 
-    public static void main(String[] args) {
+    @Autowired
+    public AirlineRatingApplication(Environment environment) {
+        AirlineRatingApplication.environment = environment;
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(AirlineRatingApplication.class, args);
+        AirlineRatingApplication da = new AirlineRatingApplication(environment);
+        da.contextLoads();
+    }
+
+    public void contextLoads() throws Exception {
+        System.out.println("DemoApplication 실행");
+        System.out.println("profile 값 :: " + environment.getProperty("spring.profiles.active"));
+        System.out.println("profile 값 environment :: " + environment);
+        String username = environment.getProperty("spring.test.username");
+        System.out.println("USERNAME :: " + username);
     }
 
 }
